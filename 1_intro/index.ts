2c65477eb130d795260321c15d5a6c4b745061ec
Davimteixeira <daviteixeira077@gmail.com>
2024-08-26 19:52:55 -0300

    Introdução ao type-script

---
+++ 1_intro/index.ts
@@ -0,0 +1,11 @@
+const firstName = "Davi"
+const anotherName = 1
+const x = true
+
+function greeting(name: string) {
+    console.log("olá " + name)
+}
+
+greeting(firstName)
+// greeting(anotherName)
+// greeting(x)
